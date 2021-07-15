32af7776fcc2abc09d18acd9b9c6804f18adf875
CaioHTeixeira <caioteixeiraaa@gmail.com>
2021-07-14 21:28:26 -0300

    refactoring

--- app.js
+++ app.js
@@ -2,42 +2,55 @@ const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
 const inputSearchTodo = document.querySelector('.form-search input')
 
+const addTodo = inputValue => (
+    `<li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+     </li>`
+)
+
+const resetFormAddTodo = () => event.target.reset()
+
+const hideTodo = todo => {
+    todo.classList.remove('d-flex')
+    todo.classList.add('hidden')
+}
+
+const showTodo = todo => {
+    todo.classList.remove('hidden')
+    todo.classList.add('d-flex')
+}
+
 formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
 
     const inputValue = event.target.add.value.trim()
+    
     if (inputValue.length) {
-        todosContainer.innerHTML += `
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                <span>${inputValue}</span>
-                <i class="far fa-trash-alt delete"></i>
-            </li>
-        `
-        event.target.reset()
-    }
+        todosContainer.innerHTML += addTodo(inputValue)
+        resetFormAddTodo()
+    } 
 })
 
 todosContainer.addEventListener('click', event => {
     const clickedElement = event.target
-
-    if (Array.from(clickedElement.classList).includes('delete')) {
-        clickedElement.parentNode.remove()
-    }
+    
+    if (clickedElement.dataset.trash) 
+        document.querySelector(`[data-todo="${clickedElement.dataset.trash}"]`)
+            .remove()
 })
 
 inputSearchTodo.addEventListener('input', event => {
     const inputValue = event.target.value.trim().toLowerCase()
     const lis = Array.from(todosContainer.children)
-    
+
     lis.filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
         .forEach(todo => {
-            todo.classList.remove('d-flex')
-            todo.classList.add('hidden')
+            hideTodo(todo)
         })
-    
+
     lis.filter(todo => todo.textContent.toLowerCase().includes(inputValue))
         .forEach(todo => {
-        todo.classList.remove('hidden')
-        todo.classList.add('d-flex')
+            showTodo(todo)
         })
 })
